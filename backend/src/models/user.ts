a0f958b4974d6ccbe1716f36344d687adb5de0a6
chiragsingla014 <chiragsingla014@protonmail.com>
2025-04-06 15:26:08 +0530

    create models, validations and types

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,7 +2,6 @@ import mongoose, { model, Schema } from 'mongoose';
 import { IUser } from '../types/types';
 
 
-
 const UserSchema = new Schema<IUser>({
     username: {type: String ,required: true, trim: true, unique: true, minLength: 3, maxLength: 30, lowercase: true},
     password: {type: String, required: true, minLength: 6},})
